0c9fd1eb98bfffbfc9f2ec36766ef30befa16024
Phyberosis <Phyberosis@gmail.com>
2017-02-25 20:57:48 -0800

    gui working

---
+++ GameProject- Wonny Invaders/src/gui/Window.java
@@ -0,0 +1,33 @@
+package gui;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends Canvas
+{
+
+	/**
+	 *  a bare bones window
+	 */
+	private static final long serialVersionUID = -4810618286807932601L;
+	
+	public Window(int w, int h, String title, Game g)
+	{
+		JFrame f = new JFrame(title);
+		
+		Dimension d = new Dimension(w, h);
+		f.setPreferredSize(d);
+		f.setMaximumSize(d);
+		f.setMinimumSize(d);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLocationRelativeTo(null);
+		f.add(g);
+		f.setVisible(true);
+		g.start();
+	}
+	
+}
